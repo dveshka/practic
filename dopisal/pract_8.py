1eddfbf374cf26b81bcaeff04dab5c54a2cbe40d
dveshka <kfc.torrent@bk.ru>
2026-03-31 11:36:52 +0300

    del
    del

--- dopisal/pract_8.py
+++
@@ -1,6 +0,0 @@
-string = input("Введите строку: ")
-
-newString = string.split()
-result = " ".join(newString)
-print(string)
-print(result)
--- dopisal/pract_9.py
+++
@@ -1,9 +0,0 @@
-
-question = "Столица России?"
-answer = "Россия"
-
-user = input(f"{question}\nВаш ответ: ")
-if user.strip().lower() == answer.lower():
-    print("Верно! Вы молодец.")
-else:
-    print(f"Ошибка. Правильный ответ: {answer}")
